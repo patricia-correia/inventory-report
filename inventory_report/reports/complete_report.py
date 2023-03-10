146461f43d056a07a5a6ab05e1928b38fc5eaeb5
Patricia Correia <pncorreia25@gmail.com>
2023-03-10 14:30:16 -0300

    gerando relatorio completo

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data):
+        report = SimpleReport.generate(data)
+        company_products = Counter(
+            [company["nome_da_empresa"] for company in data]
+        ).most_common()
+
+        most_products = ""
+        for company, count in company_products:
+            most_products += f"- {company}: {count}\n"
+
+        return (
+            f"{report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{most_products}"
+        )
