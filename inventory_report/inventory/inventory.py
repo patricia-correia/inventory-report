de942088358f3fe0790ee0601f75f7baa57f5dc8
Patricia Correia <pncorreia25@gmail.com>
2023-03-13 11:59:58 -0300

    importa arquivo csv, xml e json

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,39 @@
 import csv
+import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
     @staticmethod
-    def import_data(url, type):
-        complete_list = Inventory.read_file_csv(url)
-        if type == 'simples':
-            return SimpleReport.generate(complete_list)
-        return CompleteReport.generate(complete_list)
+    def import_data(path, type):
+        reports_list = Inventory.read_file_csv(path)
+        if type == "simples":
+            return SimpleReport.generate(reports_list)
+        return CompleteReport.generate(reports_list)
 
-    def read_file_csv(url):
-        with open(url, "r") as file:
+    def read_file_csv(path):
+        with open(path, "r") as file:
             reader = csv.DictReader(file, delimiter=",", quotechar='"')
             return list(reader)
+
+    def read_file_json(path):
+        with open(path) as file:
+            reader = file.read()
+            return json.loads(reader)
+
+    def read_file_xml(path):
+        with open(path) as file:
+            reader = file.read()
+            return xmltodict.parse(reader)["dataset"]["record"]
+
+    def indent_type_file(path):
+        if path.endswith(".csv"):
+            return Inventory.read_file_csv(path)
+
+        if path.endswith(".json"):
+            return Inventory.read_file_json(path)
+
+        if path.endswith(".xml"):
+            return Inventory.read_file_xml(path)
