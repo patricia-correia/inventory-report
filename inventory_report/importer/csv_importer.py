932934a3b3344d31103ed7542ea2672ffab851e6
Patricia Correia <pncorreia25@gmail.com>
2023-03-13 11:21:42 -0300

    tentando passar o req 4

--- inventory_report/importer/csv_importer.py
+++ inventory_report/importer/csv_importer.py
@@ -5,13 +5,13 @@ from inventory_report.reports.complete_report import CompleteReport
 
 class Inventory:
     @staticmethod
-    def read_file_csv(file_path):
-        with open(file_path, "r") as file:
-            reader = csv.DictReader(file, delimiter=",", quotechar='"')
-            return list(reader)
-
     def import_data(file_path, type):
         report_list = Inventory.read_file_csv(file_path)
         if type == 'simples':
             return SimpleReport.generate(report_list)
         return CompleteReport.generate(report_list)
+
+    def read_file_csv(file_path):
+        with open(file_path, "r") as file:
+            reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            return list(reader)
