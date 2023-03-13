95a68318546ada72e5ee5fa2d89291d05bd84e23
Patricia Correia <pncorreia25@gmail.com>
2023-03-13 11:15:37 -0300

    importação do csv

---
+++ inventory_report/importer/csv_importer.py
@@ -0,0 +1,17 @@
+import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
+class Inventory:
+    @staticmethod
+    def read_file_csv(file_path):
+        with open(file_path, "r") as file:
+            reader = csv.DictReader(file, delimiter=",", quotechar='"')
+            return list(reader)
+
+    def import_data(file_path, type):
+        report_list = Inventory.read_file_csv(file_path)
+        if type == 'simples':
+            return SimpleReport.generate(report_list)
+        return CompleteReport.generate(report_list)
